f8b2bda69e3d07f141aff11eddd2247de967eceb
RIKU-SEINO <rikuseino33@gmail.com>
2024-07-11 20:55:17 +0900

    create system spec for editing project

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -4,9 +4,6 @@ RSpec.describe "Projects", type: :system do
 
   scenario "user creates a new project" do
     user = FactoryBot.create(:user)
-    # using our custom login helper:
-    # sign_in_as user
-    # or the one provided by Devise:
     sign_in user
 
     visit root_path
@@ -16,12 +13,32 @@ RSpec.describe "Projects", type: :system do
       fill_in "Name", with: "Test Project"
       fill_in "Description", with: "Trying out Capybara"
       click_button "Create Project"
-
-      aggregate_failures do
-        expect(page).to have_content "Project was successfully created"
-        expect(page).to have_content "Test Project"
-        expect(page).to have_content "Owner: #{user.name}"
-      end
     }.to change(user.projects, :count).by(1)
+
+    aggregate_failures do
+      expect(page).to have_content "Project was successfully created"
+      expect(page).to have_content "Test Project"
+      expect(page).to have_content "Owner: #{user.name}"
+    end
+  end
+
+  scenario "user edits a project" do
+    user = FactoryBot.create(:user)
+    project = FactoryBot.create(:project, name: "RSpec tutorial", owner: user)
+    sign_in user
+
+    visit root_path
+
+    click_link "RSpec tutorial"
+
+    click_link "Edit"
+
+    fill_in "Name", with: "Edited RSpec tutorial"
+    fill_in "Description", with: "Edited Description"
+
+    click_button "Update Project"
+
+    expect(page).to have_css "div.alert.alert-success", text: "Project was successfully updated."
+    expect(project.reload.name).to eq "Edited RSpec tutorial"
   end
 end
